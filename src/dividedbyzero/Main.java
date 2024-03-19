72626a03906d9f8b5c085df9d6b43ec0c5394b32
Think <1925572868@qq.com>
2024-03-19 17:36:30 +0800

    Main2

--- src/dividedbyzero/Main.java
+++ src/dividedbyzero/Main.java
@@ -1,4 +1,4 @@
-package dividedbyzero;
+package dividebyzero;
 public class Main {
 
     /**
---
+++ src/text1/Main.java
@@ -0,0 +1,12 @@
+package text1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(3 / 0);
+        } catch (Exception e) {
+            System.out.printf("Caught runtime exception = %s\n", e.getMessage());
+        }
+    }
+}
