a0196d389c02a267e6a62bd0908241d7a2c03bd5
Think <1925572868@qq.com>
2024-03-19 17:26:52 +0800

    Main

--- src/dividedbyzero/Main.java
+++ src/dividedbyzero/Main.java
@@ -1,4 +1,11 @@
 package dividedbyzero;
-
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(3/0);
+    }
+
 }
